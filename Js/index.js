5353899bff7e89bba8f4d52f6fb9e2789241030a
Stevan-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-11-18 17:34:26 +0700

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -11,9 +11,40 @@ function storeList() {
 function getTodos() {
   if (window.localStorage.todoList) {
     list.innerHTML = window.localStorage.todoList;
+    settingTodo();
   } else {
     return;
   }
+}
+// settingTodo
+
+function settingTodo() {
+  const btn = document.querySelectorAll(".btn");
+  const todo = document.querySelectorAll(".todo");
+  const cross = document.querySelectorAll(".cross");
+
+  //for each btn
+  btn.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      if (element.parentElement.classList.contains("btnClicked"))
+        element.parentElement.classList.remove("btnClicked");
+      else {
+        element.parentElement.classList.add("btnClicked");
+      }
+    });
+  });
+
+  //for each cross => deleted
+  cross.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      if (element.parentElement.classList.contains("btnClicked")) {
+        element.parentElement.remove(list.innerHTML);
+        storeList();
+      } else {
+        return;
+      }
+    });
+  });
 }
 
 //inject lign todo & html
@@ -21,9 +52,17 @@ function addTodo() {
   list.innerHTML += `
   <li class="todo">
         <button class="btn"></button>
-        ${input.value}</li>`;
-
+        <p class="text"> ${input.value}</p>
+        <svg class="cross"></svg>
+  </li>`;
   storeList();
+  settingTodo();
+}
+
+//button clicked
+
+function buttonClicked() {
+  console.log("hello world");
 }
 
 /*************************************AddEventListener************************************/
@@ -41,5 +80,4 @@ form.addEventListener("submit", (e) => {
     addTodo(input.value);
     input.value = "";
   }
-  return;
 });
