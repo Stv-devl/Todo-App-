c8372e8f08e8970541bc745db94312a13675ecd7
Stevan-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-11-26 11:12:00 +0700

    Add files via upload

--- Js/index.js
+++ Js/index.js
@@ -7,21 +7,31 @@ const listing = document.querySelectorAll(".listing");
 const LocalStorageList = "taskTodos";
 let todos = JSON.parse(localStorage.getItem(LocalStorageList)) || [];
 
-// create todo array with id, name and completed
+displayNumberOfTodo();
+
+// create todo array with id, name and completed statut
 function createTodo() {
   let id = todos.length + 1;
   return { id: id, name: input.value, completed: false };
 }
 
-// add todo in todos array, save to storage, launch display and setting
-function addTodo() {
+//function calculate
+function calculate() {
+  for (let i = 0; i < todos.length; i++) {
+    let item = todos[i];
+  }
+}
+
+// add todo in todos array, save to storage, launch display and setting functions
+function createTodoArray() {
   todos.push(createTodo());
   saveToLocalstorage();
   displayTodo();
+  displayNumberOfTodo();
   settingTodo();
 }
 
-//display todo
+//display todo on page
 function displayTodo() {
   let listNumber = todos.length - 1;
 
@@ -32,13 +42,15 @@ function displayTodo() {
 </li>`;
 }
 
-//display todo at reload && when we delete li
+//display todo at reload && when we delete one <li></li>
 function getTodos() {
   if (window.localStorage.taskTodos) {
-    list.innerHTML = ""; // when we delete 1 li we will delete list.innerHtml for write it again
+    list.innerHTML = ""; // when we delete one <li></li> we will delete all the <li></li> for write it again, if not it's write 2 times
+
     for (let i = 0; i < todos.length; i++) {
       let item = todos[i];
 
+      //for each <li></li>
       list.innerHTML += `<li class="todo" id="${item.id}">
         <button class="btn"></button>
         <p class="text"> ${item.name}</p>
@@ -52,13 +64,21 @@ function getTodos() {
   return;
 }
 
-//keep classList BtnClicked when reload
+//when the page is reload we have to keep classList = "todo BtnClicked", so if completed is true we add btnClicked at reload
 function relaunchBtnCLicked(i) {
   const todo = document.querySelectorAll(".todo");
   if (todos[i].completed == true) {
     todo[i].classList = "todo btnClicked";
   }
   return;
+}
+
+//display number of todo left
+function displayNumberOfTodo() {
+  const filterFalse = todos.filter(function (falseFilter) {
+    return falseFilter.completed == false;
+  });
+  numberDisplay.innerHTML = filterFalse.length;
 }
 
 // save data to local storage
@@ -71,7 +91,7 @@ function settingTodo() {
   const btn = document.querySelectorAll(".btn");
   const cross = document.querySelectorAll(".cross");
 
-  //for each btn => when we check button : completed become "true", and we add btnClicked to classList. => when we uncheck completed become "false" and we remove btnClicked to class list
+  //for each btn => when we click on the button : completed become "true", and we add btnClicked to classList. => when we uncheck button, completed become "false" and we remove btnClicked to classlist
   btn.forEach((element) => {
     element.addEventListener("click", (e) => {
       let elementId = element.parentElement.id;
@@ -79,15 +99,17 @@ function settingTodo() {
       if (element.parentElement.classList.contains("btnClicked")) {
         element.parentElement.classList.remove("btnClicked");
         todos[elementId - 1].completed = false;
+        displayNumberOfTodo();
         saveToLocalstorage();
       } else {
         element.parentElement.classList.add("btnClicked");
         todos[elementId - 1].completed = true;
+        displayNumberOfTodo();
         saveToLocalstorage();
       }
     });
   });
-  //for each cross => when we click on the cross we delete the innerHtml, and we remove array from localStorage
+  //for each cross => when we click on the cross we delete the innerHtml and we remove the array from localStorage
   cross.forEach((element) => {
     element.addEventListener("click", (e) => {
       if (element.parentElement.classList.contains("btnClicked")) {
@@ -99,7 +121,8 @@ function settingTodo() {
 
         updateId();
         saveToLocalstorage();
-        getTodos(); //we relaunch getTodos for change Id number in html after delete 1 todo
+        getTodos(); //we launch getTodos for change id number in the <li></li> after delete 1 todo
+        displayNumberOfTodo();
 
         return;
       }
@@ -107,7 +130,7 @@ function settingTodo() {
   });
 }
 
-// update Id number when we remove a todo from list
+// we update the id number after removing a todo from list
 function updateId() {
   for (i = 0; i < todos.length; i++) {
     todos[i].id = i + 1;
@@ -116,17 +139,17 @@ function updateId() {
 
 /*************************************AddEventListener************************************/
 
-//when page is load send to storage
+//when page is reload send start launch getTodo()
 window.addEventListener("load", getTodos);
 
-//sumbit todo
+//write todo in input
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (input.value === "") {
     return;
   } else {
-    addTodo(input.value);
+    createTodoArray(input.value);
     input.value = "";
   }
 });
